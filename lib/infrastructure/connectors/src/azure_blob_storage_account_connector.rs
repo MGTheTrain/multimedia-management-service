d55a9eb9612333cf2d01b06a248ebf2291f80951
root <you@example.com>
2023-10-07 15:38:23 +0200

    update azure storage account connector async methods and update integration tests

--- lib/infrastructure/connectors/src/azure_blob_storage_account_connector.rs
+++ lib/infrastructure/connectors/src/azure_blob_storage_account_connector.rs
@@ -3,7 +3,7 @@ use std::{io::{BufReader, Read, Write}, fs::{File, self}};
 use azure_core::Error;
 use azure_storage::prelude::*;
 use azure_storage_blobs::prelude::*;
-
+use log::{info};
 pub struct AzureBlobStorageAccountConnector {
     container_client: Option<ContainerClient>,
 }
@@ -23,36 +23,46 @@ impl AzureBlobStorageAccountConnector {
         }
     }
 
-    /// Private helper method for returning a blob client
+    /// Private method for returning a blob client
     /// 
     /// This method takes &self and the blob_name,
     /// and returns an BlobClient object
-    pub(crate) fn get_blob_client(&self, blob_name: &str) -> BlobClient {
+    pub(crate) fn get_blob_client(&self, blob_name: &str) -> Option<BlobClient> {
         let blob_client = self.container_client.as_ref().unwrap().blob_client(blob_name);
-        blob_client
+        Some(blob_client)
     }
 
-    /// Method for uploading blobs to a Storage Account Container
+    /// Async method for uploading blobs to an Azure Storage Account Container
     ///
     /// This method takes &self, the blob_name and an file_path,
-    /// and returns an Result object
+    /// and returns an Result<(), Error> object
     async fn upload_blob(&self, blob_name: &str, file_path: &str) -> Result<(), Error> {
-        let blob_client = self.get_blob_client(blob_name);
+        let blob_client = self.get_blob_client(blob_name).unwrap();
         let f = File::open(file_path)?;
         let mut reader = BufReader::new(f);
         let mut buffer = Vec::new();
         reader.read_to_end(&mut buffer)?;
         blob_client.put_block_blob(buffer).await?;
+        info!("Successfully uploaded blob {}", blob_name);
         Ok(())
     }
 
-    /// Method for downloading blobs to a Storage Account Container
+    /// Async method for retrieving the content of a blob from an Azure Storage Account Container
+    ///
+    /// This method takes &self and the blob_name,
+    /// and returns an Result<Vec<u8>, Error> object
+    async fn retrieve_bytes(&self, blob_name: &str) -> Result<Vec<u8>, Error> {
+        let blob_client = self.get_blob_client(blob_name).unwrap();
+        let data = blob_client.get_content().await?;
+        Ok(data)
+    }
+
+    /// Async method for downloading blobs from an Azure Storage Account Container
     ///
     /// This method takes &self, the blob_name and an file_path,
-    /// and returns an Result object
+    /// and returns an Result<(), Error> object
     async fn download_blob(&self, blob_name: &str, file_path: &str) -> Result<(), Error> {
-        let blob_client = self.get_blob_client(blob_name);
-        let data = blob_client.get_content().await?;
+        let data = self.retrieve_bytes(blob_name).await?;
 
         let mut file = fs::OpenOptions::new()
             .create(true) 
@@ -60,12 +70,18 @@ impl AzureBlobStorageAccountConnector {
             .open(file_path)?;
     
         file.write_all(&data)?;
+        info!("Successfully downloaded blob {}", blob_name);
         Ok(())
     }
 
-    async fn delete_blob(&self, blob_name: &str) -> Result<(), Box<Error>> {
-        let blob_client = self.get_blob_client(blob_name);
+    /// Async method for deleting blobs from an Azure Storage Account Container
+    ///
+    /// This method takes &self, the blob_name,
+    /// and returns an Result<(), Error> object
+    async fn delete_blob(&self, blob_name: &str) -> Result<(), Error> {
+        let blob_client = self.get_blob_client(blob_name).unwrap();
         blob_client.delete().await?;
+        info!("Successfully deleted blob {}", blob_name);
         Ok(())
     }
 }
@@ -74,11 +90,28 @@ impl AzureBlobStorageAccountConnector {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_azure_storage_account_container_connector_methods() {
-        let azure_blob_storage_account_connector = 
-            Box::new(AzureBlobStorageAccountConnector::new("","",""));  
-        
+    // In order to run the test execute: `RUST_LOG=info cargo test`
+    #[tokio::test]
+    async fn test_azure_storage_account_container_connector_methods() -> azure_core::Result<()> {
+        env_logger::init();
 
+        let env_file_path = "./assets/azure-secrets.dev.cfg";
+        dotenv::from_path(env_file_path).ok();
+
+        let azure_access_key = std::env::var("AZURE_ACCESS_KEY").expect("AZURE_ACCESS_KEY not found in .cfg");
+        let azure_account_name = std::env::var("AZURE_ACCOUNT_NAME").expect("AZURE_ACCOUNT_NAME not found in .cfg");
+        let azure_container_name = std::env::var("AZURE_CONTAINER_NAME").expect("AZURE_CONTAINER_NAME not found in .cfg");
+
+        let azure_blob_storage_account_connector = 
+            Box::new(AzureBlobStorageAccountConnector::new(
+                azure_account_name.as_str(), azure_access_key.as_str(), azure_container_name.as_str()));  
+        
+        let upload_file_path = "./assets/sample.txt";
+        let download_file_path = "./temp/copy-sample.txt";
+        let blob_name = "sample.txt";
+        assert_eq!(azure_blob_storage_account_connector.upload_blob(blob_name, upload_file_path).await?, ());
+        assert_eq!(azure_blob_storage_account_connector.download_blob(blob_name, download_file_path).await?, ());
+        assert_eq!(azure_blob_storage_account_connector.delete_blob(blob_name).await?, ());
+        Ok(())
     }
 }
