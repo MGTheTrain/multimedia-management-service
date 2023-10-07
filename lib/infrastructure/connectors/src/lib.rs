fe2588483b35129bff759d1dfcda15c071dc0c76
root <you@example.com>
2023-10-07 22:39:07 +0200

    remove overhead

--- lib/infrastructure/connectors/src/lib.rs
+++ lib/infrastructure/connectors/src/lib.rs
@@ -1,17 +1,3 @@
 // pub mod blob_storage_connector;
 pub mod azure_blob_storage_account_connector;
 pub mod aws_s3_bucket_connector;
-
-pub fn hello() {
-    println!("Hello from infrastructure connectors!");
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_hello() {
-        hello();
-    }
-}
