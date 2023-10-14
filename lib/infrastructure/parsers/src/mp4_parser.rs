34e700ad0c4621d85a6336beb5702fe4404893db
Marvin Gajek <Marvin.Gajek1@web.de>
2023-10-15 00:33:18 +0200

    update mp4 parser implementation

--- lib/infrastructure/parsers/src/mp4_parser.rs
+++ lib/infrastructure/parsers/src/mp4_parser.rs
@@ -5,141 +5,95 @@ use std::io::{self, BufReader};
 use std::path::Path;
 
 use mp4::{Error, Mp4Track, Result, TrackType};
-use crate::models;
+use models::{
+    container_meta::ContainerMeta,
+    track::{self, VideoTrack, AudioTrack, SubtitleTrack},
+};
 
 pub struct Mp4Parser {} 
 
 impl Mp4Parser {
-    fn info(filename: &str) -> Result<Vec<models::track::Track>, Box<dyn std::error::Error>> {
+    /// Method for retrieving MP4 file info
+    ///
+    /// Requires the filename as a parameter and returns a 
+    /// Result<(ContainerMeta, VideoTrack, AudioTrack, SubtitleTrack), Box<dyn std::error::Error>> object   
+    fn info(filename: &str) -> Result<(ContainerMeta, VideoTrack, AudioTrack, SubtitleTrack), Box<dyn std::error::Error>> {
         let f = File::open(filename)?;
         let size = f.metadata()?.len();
         let reader = BufReader::new(f);
 
         let mp4 = mp4::Mp4Reader::read_header(reader, size)?;
 
+        let container_meta = ContainerMeta::new();
         
-    }
-}
+        container_meta.id = Uuid::new_v4();
+        container_meta.date_time_created = Utc::now();
+        container_meta.date_time_updated = container_meta.date_time_created;
+        container_meta.file_size_in_kb = mp4.size();
+        container_meta.duration = mp4.duration();
+        // container_meta.title = String::from(""); // not set here
+        // container_meta.description = String::from(""); // not set here
+        // container_meta.tags = Vec::new(); // not set here
 
-fn info<P: AsRef<Path>>(filename: &P) -> Result<()> {
-    let f = File::open(filename)?;
-    let size = f.metadata()?.len();
-    let reader = BufReader::new(f);
-
-    let mp4 = mp4::Mp4Reader::read_header(reader, size)?;
-
-    println!("File:");
-    println!("  file size:          {}", mp4.size());
-    println!("  major_brand:        {}", mp4.major_brand());
-    let mut compatible_brands = String::new();
-    for brand in mp4.compatible_brands().iter() {
-        compatible_brands.push_str(&brand.to_string());
-        compatible_brands.push(' ');
-    }
-    println!("  compatible_brands:  {}\n", compatible_brands);
-
-    println!("Movie:");
-    println!("  version:        {}", mp4.moov.mvhd.version);
-    println!(
-        "  creation time:  {}",
-        creation_time(mp4.moov.mvhd.creation_time)
-    );
-    println!("  duration:       {:?}", mp4.duration());
-    println!("  fragments:      {:?}", mp4.is_fragmented());
-    println!("  timescale:      {:?}\n", mp4.timescale());
-
-    println!("Found {} Tracks", mp4.tracks().len());
-    for track in mp4.tracks().values() {
-        let media_info = match track.track_type()? {
-            TrackType::Video => video_info(track),
-            TrackType::Audio => audio_info(track),
-            TrackType::Subtitle => subtitle_info(track),
-        };
-
-        println!(
-            "  Track: #{}({}) {}: {}",
-            track.track_id(),
-            track.language(),
-            track.track_type()?,
-            media_info.unwrap_or_else(|e| e.to_string())
-        );
-    }
-    Ok(())
-}
-
-fn video_info(track: &Mp4Track) -> Result<String> {
-    if track.trak.mdia.minf.stbl.stsd.avc1.is_some() {
-        Ok(format!(
-            "{} ({}) ({:?}), {}x{}, {} kb/s, {:.2} fps",
-            track.media_type()?,
-            track.video_profile()?,
-            track.box_type()?,
-            track.width(),
-            track.height(),
-            track.bitrate() / 1000,
-            track.frame_rate()
-        ))
-    } else {
-        Ok(format!(
-            "{} ({:?}), {}x{}, {} kb/s, {:.2} fps",
-            track.media_type()?,
-            track.box_type()?,
-            track.width(),
-            track.height(),
-            track.bitrate() / 1000,
-            track.frame_rate()
-        ))
-    }
-}
-
-fn audio_info(track: &Mp4Track) -> Result<String> {
-    if let Some(ref mp4a) = track.trak.mdia.minf.stbl.stsd.mp4a {
-        if mp4a.esds.is_some() {
-            let profile = match track.audio_profile() {
-                Ok(val) => val.to_string(),
-                _ => "-".to_string(),
+        //tracks
+        for track in mp4.tracks().values() {
+            let media_info = match track.track_type()? {
+                TrackType::Video => video_info(track),
+                TrackType::Audio => audio_info(track),
+                TrackType::Subtitle => subtitle_info(track),
             };
-
-            let channel_config = match track.channel_config() {
-                Ok(val) => val.to_string(),
-                _ => "-".to_string(),
-            };
-
-            Ok(format!(
-                "{} ({}) ({:?}), {} Hz, {}, {} kb/s",
-                track.media_type()?,
-                profile,
-                track.box_type()?,
-                track.sample_freq_index()?.freq(),
-                channel_config,
-                track.bitrate() / 1000
-            ))
-        } else {
-            Ok(format!(
-                "{} ({:?}), {} kb/s",
-                track.media_type()?,
-                track.box_type()?,
-                track.bitrate() / 1000
-            ))
         }
-    } else {
-        Err(Error::InvalidData("mp4a box not found"))
-    }
-}
 
-fn subtitle_info(track: &Mp4Track) -> Result<String> {
-    if track.trak.mdia.minf.stbl.stsd.tx3g.is_some() {
-        Ok(format!("{} ({:?})", track.media_type()?, track.box_type()?,))
-    } else {
-        Err(Error::InvalidData("tx3g box not found"))
-    }
-}
 
-fn creation_time(creation_time: u64) -> u64 {
-    // convert from MP4 epoch (1904-01-01) to Unix epoch (1970-01-01)
-    if creation_time >= 2082844800 {
-        creation_time - 2082844800
-    } else {
-        creation_time
+        // container_meta.video_track_id = Uuid::nil(),
+        // container_meta.audio_track_id = Uuid::nil(),
+        // container_meta.subtitle_track_id = Uuid::nil(),
+
+        Ok(container_meta,)
+    }
+
+    /// Helper method for the video section of the MP4 file if available
+    ///
+    /// Requires the filename as a parameter and returns a 
+    /// Result<Option<VideoTrack>, Box<dyn std::error::Error>> object   
+    fn video_info(track: &Mp4Track, video_track: &VideoTrack) -> Result<Option<VideoTrack>, Box<dyn std::error::Error>> {
+        let video_track = VideoTrack::new();
+        video_track.media_type = track.media_type()?;
+        video_track.width = track.width();
+        video_track.height = track.height();
+        video_track.bit_rate = track.bitrate() / 1000;
+        video_track.frame_rate = track.frame_rate();
+        
+        Ok(None)
+    }
+
+    
+    /// Helper method for the audio section of the MP4 file if available
+    ///
+    /// Requires the filename as a parameter and returns a 
+    /// Result<Option<AudioTrack>, Box<dyn std::error::Error>> object   
+    fn audio_info(track: &Mp4Track, audio_track: &AudioTrack) -> Result<Option<AudioTrack>, Box<dyn std::error::Error>> {
+        let audio_track = AudioTrack::new();
+        audio_track.media_type = track.media_type()?;        
+        audio_track.bit_rate = track.bitrate() / 1000;   
+        let channel_config = match track.channel_config() {
+            Ok(val) => val.to_string(),
+            _ => "-".to_string(),
+        };     
+        audio_track.channel_config = channel_config;
+        audio_track.sample_frequenz = track.sample_freq_index()?.freq();
+        
+        Ok(None)
+    }
+
+    /// Helper method for the subtitle section of the MP4 file if available
+    /// 
+    /// Requires the filename as a parameter and returns a 
+    /// Result<Option<SubtitleTrack>, Box<dyn std::error::Error>> object   
+    fn audio_info(track: &Mp4Track, subtitle_track: &SubtitleTrack) -> Result<Option<SubtitleTrack>, Box<dyn std::error::Error>> {
+        let subtitle_track = SubtitleTrack::new();
+        subtitle_track.media_type = track.media_type()?;        
+
+        Ok(None)
     }
 }
