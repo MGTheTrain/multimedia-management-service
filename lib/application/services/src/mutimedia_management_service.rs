ee329b4b006a580b0fe665373a886ac395da86bc
Marvin Gajek <Marvin.Gajek1@web.de>
2023-10-14 11:43:23 +0200

    update rs files and cargo.toml

--- lib/application/services/src/mutimedia_management_service.rs
+++ lib/application/services/src/mutimedia_management_service.rs
@@ -1,7 +1,37 @@
-struct MutimediaManagementService {
+extern crate connectors;
+extern crate data_access;
 
+struct MutimediaManagementService {
+    connector: connectors::aws_s3_bucket_connector::AwsS3BucketConnector,
+    data_access: data_access::psql_data_access_async::PsqlDataAccess,
 }
 
 impl MutimediaManagementService {
-    
+    /// Method for creating the MutimediaManagementService constructor
+    ///
+    /// Requires no parameters and returns and MutimediaManagementService object
+    async fn new() -> Self {
+        MutimediaManagementService {
+            connector: connectors::aws_s3_bucket_connector::AwsS3BucketConnector::new().await,
+            data_access: data_access::psql_data_access_async::PsqlDataAccess::new(),
+        }
+    }
+
+    /// Method for uploading blobs to a blob storage and inserting for the uploaded file file_meta rows into a Psql database table utilizing diesel ORM
+    ///
+    /// Requires a bucket_name, a key, a file_name and returns a Result<models::file_meta::FileMeta, diesel::result::Error>
+    async fn upload_blob(        
+        bucket_name: &str,
+        key: &str,
+        file_name: &str) -> Result<(), Box<dyn std::error::Error>> {
+            
+
+
+        Ok(())
+    }    
+
+    async fn download_blob_by_id() -> Result<(), Box<dyn std::error::Error>> {
+
+        Ok(())
+    }    
 }
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
+use models::model::Model;
+
 extern crate connectors;
 extern crate controllers;
 extern crate data_access;
 extern crate dtos;
-extern crate interfaces;
 extern crate models;
 extern crate services;
 
 fn main() {
-    models::hello();
-    interfaces::hello();
-    controllers::hello();
-    dtos::hello();
-    services::hello();
-    connectors::hello();
-    settings::hello();
-    data_access::hello();
+    let container_meta = models::container_meta::ContainerMeta::new();
+    // controllers::hello();
+    // dtos::hello();
+    // services::hello();
+    // connectors::hello();
+    // data_access::hello();
 }
